c2101d785d9f3db5ce02a0bdd3fc82d3784fc1f8
Balazs Vadnai <11341356+bvadnai@users.noreply.github.com>
2024-01-15 20:36:21 +0100

    Remove whitespaces

--- Sources/BinUtils.swift
+++ Sources/BinUtils.swift
@@ -23,17 +23,17 @@ extension Double: Unpackable {}
 protocol DataConvertible {}
 
 extension DataConvertible {
-    
+
     init?(data: Data) {
         guard data.count == MemoryLayout<Self>.size else { return nil }
         self = data.withUnsafeBytes { $0.load(as: Self.self) }
     }
-    
+
     init?(bytes: [UInt8]) {
         let data = Data(bytes)
         self.init(data:data)
     }
-    
+
     var data: Data {
         var value = self
         return withUnsafePointer(to: &value) { Data(bytes: UnsafePointer($0), count: MemoryLayout<Self>.size) }
@@ -74,42 +74,42 @@ extension Data {
 // MARK: functions
 
 public func hexlify(_ data:Data) -> String {
-    
+
     // similar to hexlify() in Python's binascii module
     // https://docs.python.org/2/library/binascii.html
-    
+
     var s = String()
     var byte: UInt8 = 0
-    
+
     for i in 0 ..< data.count {
         NSData(data: data).getBytes(&byte, range: NSMakeRange(i, 1))
         s = s.appendingFormat("%02x", byte)
     }
-    
+
     return s as String
 }
 
 public func unhexlify(_ string:String) -> Data? {
-    
+
     // similar to unhexlify() in Python's binascii module
     // https://docs.python.org/2/library/binascii.html
-    
+
     let s = string.uppercased().replacingOccurrences(of: " ", with: "")
-    
+
     let nonHexCharacterSet = CharacterSet(charactersIn: "0123456789ABCDEF").inverted
     if let range = s.rangeOfCharacter(from: nonHexCharacterSet) {
         print("-- found non hex character at range \(range)")
         return nil
     }
-    
+
     var data = Data(capacity: s.count / 2)
-    
+
     for i in stride(from: 0, to:s.count, by:2) {
         let byteString = s[i, i+2]
         let byte = UInt8(byteString.withCString { strtoul($0, nil, 16) })
         data.append([byte] as [UInt8], count: 1)
     }
-    
+
     return data
 }
 
@@ -129,51 +129,51 @@ func readFloatingPointType<T:DataConvertible>(_ type:T.Type, bytes:[UInt8], loc:
 }
 
 func isBigEndianFromMandatoryByteOrderFirstCharacter(_ format:String) -> Bool {
-    
+
     guard let firstChar = format.first else { assertionFailure("empty format"); return false }
-    
+
     let s = NSString(string: String(firstChar))
     let c = s.substring(to: 1)
-    
+
     if c == "@" { assertionFailure("native size and alignment is unsupported") }
-    
+
     if c == "=" || c == "<" { return false }
     if c == ">" || c == "!" { return true }
-    
+
     assertionFailure("format '\(format)' first character must be among '=<>!'")
-    
+
     return false
 }
 
 // akin to struct.calcsize(fmt)
 func numberOfBytesInFormat(_ format:String) -> Int {
-    
+
     var numberOfBytes = 0
-    
+
     var n = 0 // repeat counter
-    
+
     var mutableFormat = format
-    
+
     while !mutableFormat.isEmpty {
-        
+
         let c = mutableFormat.remove(at: mutableFormat.startIndex)
-        
+
         if let i = Int(String(c)) , 0...9 ~= i {
             if n > 0 { n *= 10 }
             n += i
             continue
         }
-        
+
         if c == "s" {
             numberOfBytes += max(n,1)
             n = 0
             continue
         }
-        
+
         let repeatCount = max(n,1)
-        
+
         switch(c) {
-            
+
         case "@", "<", "=", ">", "!", " ":
             ()
         case "c", "b", "B", "x", "?":
@@ -189,10 +189,10 @@ func numberOfBytesInFormat(_ format:String) -> Int {
         default:
             assertionFailure("-- unsupported format \(c)")
         }
-        
+
         n = 0
     }
-    
+
     return numberOfBytes
 }
 
@@ -203,7 +203,7 @@ func formatDoesMatchDataLength(_ format:String, data:Data) -> Bool {
         print("format \"\(format)\" expects \(sizeAccordingToFormat) bytes but data is \(dataLength) bytes")
         return false
     }
-    
+
     return true
 }
 
@@ -221,17 +221,17 @@ public enum BinUtilsError: Error {
 }
 
 public func pack(_ format:String, _ objects:[Any], _ stringEncoding:String.Encoding=String.Encoding.windowsCP1252) -> Data {
-    
+
     var objectsQueue = objects
-    
+
     var mutableFormat = format
-    
+
     var mutableData = Data()
-    
+
     var isBigEndian = false
-    
+
     let firstCharacter = mutableFormat.remove(at: mutableFormat.startIndex)
-    
+
     switch(firstCharacter) {
     case "<", "=":
         isBigEndian = false
@@ -242,53 +242,53 @@ public func pack(_ format:String, _ objects:[Any], _ stringEncoding:String.Encod
     default:
         assertionFailure("unsupported format chacracter'")
     }
-    
+
     var n = 0 // repeat counter
-    
+
     while !mutableFormat.isEmpty {
-        
+
         let c = mutableFormat.remove(at: mutableFormat.startIndex)
-        
+
         if let i = Int(String(c)) , 0...9 ~= i {
             if n > 0 { n *= 10 }
             n += i
             continue
         }
-        
+
         var o : Any = 0
-        
+
         if c == "s" {
             o = objectsQueue.remove(at: 0)
-            
+
             guard let stringData = (o as! String).data(using: .utf8) else { assertionFailure(); return Data() }
             var bytes = stringData.bytes
-            
+
             let expectedSize = max(1, n)
-            
+
             // pad ...
             while bytes.count < expectedSize { bytes.append(0x00) }
-            
+
             // ... or trunk
             if bytes.count > expectedSize { bytes = Array(bytes[0..<expectedSize]) }
-            
+
             assert(bytes.count == expectedSize)
-            
+
             if isBigEndian { bytes = bytes.reversed() }
-            
+
             mutableData.append(bytes, count: bytes.count)
-            
+
             n = 0
             continue
         }
-        
+
         for _ in 0..<max(n,1) {
-            
+
             var bytes : [UInt8] = []
-            
+
             if c != "x" {
                 o = objectsQueue.removeFirst()
             }
-            
+
             switch(c) {
             case "?":
                 bytes = (o as! Bool) ? [0x01] : [0x00]
@@ -322,74 +322,74 @@ public func pack(_ format:String, _ objects:[Any], _ stringEncoding:String.Encod
             default:
                 assertionFailure("Unsupported packing format: \(c)")
             }
-            
+
             if isBigEndian { bytes = bytes.reversed() }
             let data = Data(bytes)
             mutableData.append(data)
         }
-        
+
         n = 0
     }
-    
+
     return mutableData
 }
 
 public func unpack(_ format:String, _ data:Data, _ stringEncoding:String.Encoding=String.Encoding.windowsCP1252) throws -> [Unpackable] {
-    
+
     assert(CFByteOrderGetCurrent() == 1 /* CFByteOrderLittleEndian */, "\(#file) assumes little endian, but host is big endian")
-    
+
     let isBigEndian = isBigEndianFromMandatoryByteOrderFirstCharacter(format)
-    
+
     if formatDoesMatchDataLength(format, data: data) == false {
         throw BinUtilsError.formatDoesMatchDataLength(format:format, dataSize:data.count)
     }
-    
+
     var a : [Unpackable] = []
-    
+
     var loc = 0
-    
+
     let bytes = data.bytes
-    
+
     var n = 0 // repeat counter
-    
+
     var mutableFormat = format
-    
+
     mutableFormat.remove(at: mutableFormat.startIndex) // consume byte-order specifier
-    
+
     while !mutableFormat.isEmpty {
-        
+
         let c = mutableFormat.remove(at: mutableFormat.startIndex)
-        
+
         if let i = Int(String(c)) , 0...9 ~= i {
             if n > 0 { n *= 10 }
             n += i
             continue
         }
-        
+
         if c == "s" {
             let length = max(n,1)
             let sub = Array(bytes[loc..<loc+length])
-            
+
             guard let s = NSString(bytes: sub, length: length, encoding: stringEncoding.rawValue) else {
                 assertionFailure("-- not a string: \(sub)")
                 return []
             }
-            
+
             a.append(s)
-            
+
             loc += length
-            
+
             n = 0
-            
+
             continue
         }
-        
+
         for _ in 0..<max(n,1) {
-            
+
             var o : Unpackable?
-            
+
             switch(c) {
-                
+
             case "c":
                 let optionalString = NSString(bytes: [bytes[loc]], length: 1, encoding: String.Encoding.utf8.rawValue)
                 loc += 1
@@ -439,12 +439,12 @@ public func unpack(_ format:String, _ data:Data, _ stringEncoding:String.Encodin
             default:
                 throw BinUtilsError.unsupportedFormat(character:c)
             }
-            
+
             if let o = o { a.append(o) }
         }
-        
+
         n = 0
     }
-    
+
     return a
 }
